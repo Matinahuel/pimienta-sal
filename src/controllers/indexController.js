a391944d45dd71bcbea5d03492148d5bc5378b36
Matinahuel <matiasnahuel2396@gmail.com>
2021-11-17 22:03:53 -0300

    pimienta&sal push

---
+++ src/controllers/indexController.js
@@ -0,0 +1,21 @@
+const platos = require('../data/platosdb')
+
+
+module.exports = {
+  index: (req, res) => {
+    return res.render('index', {
+      platos,
+    });
+
+  },
+  detail: (req, res) => {
+
+
+    let plato = platos.find(plato => plato.id === +req.params.id)
+
+    return res.render('detalleMenu',{
+      plato,
+    });
+  },
+
+}
